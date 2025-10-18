48ac75b5adb392a8c706ca3629750387a23a5df4
Jobayer Ahmed <jobayerahmed.dev@gmail.com>
2025-10-18 22:13:52 +0600

    loading component added

--- src/app/(CommonLayout)/projects/[projectId]/page.tsx
+++ src/app/(CommonLayout)/projects/[projectId]/page.tsx
@@ -19,8 +19,9 @@ import CvButton from "@/components/shared/butttons/cvButton/CvButton";
 import Image from "next/image";
 import { fetchProjectById } from "@/lib/api";
 import { IProject } from "@/models/Projects";
+import Loading from "@/components/shared/Loading";
 
-const ProjectDetailsPage = () => { 
+const ProjectDetailsPage = () => {
   const { projectId } = useParams();
 
   console.log(projectId);
@@ -50,143 +51,136 @@ const ProjectDetailsPage = () => {
     Autoplay({ delay: 4000, stopOnInteraction: true })
   );
 
-  if (!project) {
-    return (
-      <div className="p-6 text-red-500 min-h-screen">Project not found.</div>
-    );
-  }
-
   return (
     <div className=" min-h-screen text-lightGrey">
-      <div className="container mx-auto p-4">
-        <p className="py-4 flex gap-2">
-          <Link href={"/"}>
-            <button
-              type="button"
-              className="cursor-pointer text-center w-36 rounded-2xl h-8 relative text-xl font-semibold border-2 border-darkGrey group"
-            >
-              <div className="bg-mutedGrey  rounded-xl h-[28px] w-1/3 grid place-items-center absolute left-0 top-0 group-hover:w-full group-hover:text-classicGold z-10 duration-500">
-                <ArrowLeft />
-              </div>
-              <p className="translate-x-4 text-classicGold">Go Back</p>
-            </button>
-          </Link>
-        </p>
-        <Carousel
-          plugins={[plugin.current]}
-          className="w-full h-[65vh]"
-          onMouseEnter={plugin.current.stop}
-          onMouseLeave={plugin.current.reset}
-        >
-          <CarouselContent>
-            {project?.images.map((imgUrl, index) => (
-              <CarouselItem key={index} className=" min-h-[65vh] ">
-                <div className="p-1 h-full">
-                  <Card className=" h-full border-none">
-                    <CardContent className="flex items-center justify-center h-full">
-                     <div className="relative w-full h-full ">
-                       <Image
-                        alt=""
-                        fill
-                        objectFit="cover"
-                        src={imgUrl}
-                      />
-                     </div>
-                    </CardContent>
-                  </Card>
+      {!project ? (
+        <Loading height={'min-h-screen'}/>
+      ) : (
+        <div className="container mx-auto p-4 ">
+          <p className="py-4 flex gap-2">
+            <Link href={"/"}>
+              <button
+                type="button"
+                className="cursor-pointer text-center w-36 rounded-2xl h-8 relative text-xl font-semibold border-2 border-darkGrey group"
+              >
+                <div className="bg-mutedGrey  rounded-xl h-[28px] w-1/3 grid place-items-center absolute left-0 top-0 group-hover:w-full group-hover:text-classicGold z-10 duration-500">
+                  <ArrowLeft />
                 </div>
-              </CarouselItem>
-            ))}
-          </CarouselContent>
-          <CarouselPrevious />
-          <CarouselNext />
-        </Carousel>
+                <p className="translate-x-4 text-classicGold">Go Back</p>
+              </button>
+            </Link>
+          </p>
+          <Carousel
+            plugins={[plugin.current]}
+            className="w-full h-[65vh]"
+            onMouseEnter={plugin.current.stop}
+            onMouseLeave={plugin.current.reset}
+          >
+            <CarouselContent>
+              {project?.images.map((imgUrl, index) => (
+                <CarouselItem key={index} className=" min-h-[65vh] ">
+                  <div className="p-1 h-full">
+                    <Card className=" h-full border-none">
+                      <CardContent className="flex items-center justify-center h-full">
+                        <div className="relative w-full h-full ">
+                          <Image alt="" fill objectFit="cover" src={imgUrl} />
+                        </div>
+                      </CardContent>
+                    </Card>
+                  </div>
+                </CarouselItem>
+              ))}
+            </CarouselContent>
+            <CarouselPrevious />
+            <CarouselNext />
+          </Carousel>
 
-        <div className=" grid grid-cols-1 lg:grid-cols-12 gap-4 mt-4">
-          <div className="col-span-12 lg:flex  space-y-4 justify-between">
-            <h2 className="text-4xl  font-semibold">{project?.title}</h2>
-            <p className="flex gap-4">
-              <Link target="_blank" href={project?.liveLink}>
-                <CvButton
-                  className="!px-3"
-                  icon={GoLinkExternal}
-                  label="Live Link"
-                />
-              </Link>
-              <Link target="_blank" href={project?.frontendCode}>
-                <CvButton
-                  className="!px-3"
-                  icon={FiGithub}
-                  label="Frontend Code"
-                />
-              </Link>
-              <Link target="_blank" href={project?.backendCode}>
-                <CvButton
-                  className="!px-3"
-                  icon={FiGithub}
-                  label="Backend Code"
-                />
-              </Link>
-            </p>
-          </div>
-          <div className="col-span-7 space-y-4">
-            <div className=" p-4 rounded-4xl inset-shadow-xs inset-shadow-darkGrey">
-              <h3 className="text-3xl text-offWhite mb-6">Overview</h3>
-              <p>{project?.overview}</p>
+          <div className=" grid grid-cols-1 lg:grid-cols-12 gap-4 mt-4">
+            <div className="col-span-12 lg:flex  space-y-4 justify-between">
+              <h2 className="text-4xl  font-semibold">{project?.title}</h2>
+              <p className="flex gap-4">
+                <Link target="_blank" href={project?.liveLink}>
+                  <CvButton
+                    className="!px-3"
+                    icon={GoLinkExternal}
+                    label="Live Link"
+                  />
+                </Link>
+                <Link target="_blank" href={project?.frontendCode}>
+                  <CvButton
+                    className="!px-3"
+                    icon={FiGithub}
+                    label="Frontend Code"
+                  />
+                </Link>
+                <Link target="_blank" href={project?.backendCode}>
+                  <CvButton
+                    className="!px-3"
+                    icon={FiGithub}
+                    label="Backend Code"
+                  />
+                </Link>
+              </p>
             </div>
-            <div className=" p-4 rounded-4xl inset-shadow-xs inset-shadow-darkGrey">
-              <h3 className="text-3xl text-offWhite mb-6">Features</h3>
-              <ul className="list-inside list-disc">
-                {project?.features.map((feature, i) => (
-                  <li key={i}>{feature}</li>
-                ))}
-              </ul>
-            </div>
-          </div>
-          <div className="col-span-5 space-y-4">
-            <h3 className="text-3xl text-offWhite mb-6">Technologies</h3>
-            <div className=" p-4 rounded-4xl inset-shadow-xs inset-shadow-darkGrey">
-              <h3 className="text-3xl text-offWhite mb-6">Frontend</h3>
-              <div className="flex gap-3 flex-wrap">
-                {project?.technologies?.frontend.map((tec, i) => (
-                  <p
-                    key={i}
-                    className="bg-cardBg px-2.5 py-0.5 rounded-2xl text-classicGold"
-                  >
-                    {tec}
-                  </p>
-                ))}
+            <div className="col-span-7 space-y-4">
+              <div className=" p-4 rounded-4xl inset-shadow-xs inset-shadow-darkGrey">
+                <h3 className="text-3xl text-offWhite mb-6">Overview</h3>
+                <p>{project?.overview}</p>
+              </div>
+              <div className=" p-4 rounded-4xl inset-shadow-xs inset-shadow-darkGrey">
+                <h3 className="text-3xl text-offWhite mb-6">Features</h3>
+                <ul className="list-inside list-disc">
+                  {project?.features.map((feature, i) => (
+                    <li key={i}>{feature}</li>
+                  ))}
+                </ul>
               </div>
             </div>
-            <div className=" p-4 rounded-4xl inset-shadow-xs inset-shadow-darkGrey">
-              <h3 className="text-3xl text-offWhite mb-6">Backend</h3>
-              <div className="flex gap-3 flex-wrap">
-                {project?.technologies?.backend.map((tec, i) => (
-                  <p
-                    key={i}
-                    className="bg-cardBg px-2.5 py-0.5 rounded-2xl text-classicGold"
-                  >
-                    {tec}
-                  </p>
-                ))}
+            <div className="col-span-5 space-y-4">
+              <h3 className="text-3xl text-offWhite mb-6">Technologies</h3>
+              <div className=" p-4 rounded-4xl inset-shadow-xs inset-shadow-darkGrey">
+                <h3 className="text-3xl text-offWhite mb-6">Frontend</h3>
+                <div className="flex gap-3 flex-wrap">
+                  {project?.technologies?.frontend.map((tec, i) => (
+                    <p
+                      key={i}
+                      className="bg-cardBg px-2.5 py-0.5 rounded-2xl text-classicGold"
+                    >
+                      {tec}
+                    </p>
+                  ))}
+                </div>
               </div>
-            </div>
-            <div className=" p-4 rounded-4xl inset-shadow-xs inset-shadow-darkGrey">
-              <h3 className="text-3xl text-offWhite mb-6">Tools</h3>
-              <div className="flex gap-3 flex-wrap">
-                {project?.technologies?.tools.map((tec, i) => (
-                  <p
-                    key={i}
-                    className="bg-cardBg px-2.5 py-0.5 rounded-2xl text-classicGold"
-                  >
-                    {tec}
-                  </p>
-                ))}
+              <div className=" p-4 rounded-4xl inset-shadow-xs inset-shadow-darkGrey">
+                <h3 className="text-3xl text-offWhite mb-6">Backend</h3>
+                <div className="flex gap-3 flex-wrap">
+                  {project?.technologies?.backend.map((tec, i) => (
+                    <p
+                      key={i}
+                      className="bg-cardBg px-2.5 py-0.5 rounded-2xl text-classicGold"
+                    >
+                      {tec}
+                    </p>
+                  ))}
+                </div>
+              </div>
+              <div className=" p-4 rounded-4xl inset-shadow-xs inset-shadow-darkGrey">
+                <h3 className="text-3xl text-offWhite mb-6">Tools</h3>
+                <div className="flex gap-3 flex-wrap">
+                  {project?.technologies?.tools.map((tec, i) => (
+                    <p
+                      key={i}
+                      className="bg-cardBg px-2.5 py-0.5 rounded-2xl text-classicGold"
+                    >
+                      {tec}
+                    </p>
+                  ))}
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
--- src/components/home/FeaturedProjects.tsx
+++ src/components/home/FeaturedProjects.tsx
@@ -7,66 +7,71 @@ import { useEffect, useState } from "react";
 import { IProject } from "@/models/Projects";
 import { ChevronDown, ChevronRight } from "lucide-react";
 import Link from "next/link";
+import Loading from "../shared/Loading";
 const FeaturedProjects = () => {
+  const [projects, setProjects] = useState<IProject[]>([]);
+  const [showMore, setShowMore] = useState(false);
+  const [isTransitioning, setIsTransitioning] = useState(false);
 
-      const [projects, setProjects] = useState<IProject[]>([]);
-      const [showMore, setShowMore] = useState(false);
-      const [isTransitioning, setIsTransitioning] = useState(false);
-      
-      console.log(projects);
-    
-      const fetchAllProjects = async () => {
-        try {
-          const res = await fetchProjects();
-          setProjects(res);
-        } catch (error) {
-          console.error("Failed to fetch projects", error);
-        }
-      };
-    
-      useEffect(() => {
-        fetchAllProjects();
-      }, []);
-    
-      const handleToggleProjects = () => {
-        setIsTransitioning(true);
-        setTimeout(() => {
-          setShowMore(!showMore);
-          setIsTransitioning(false);
-        }, 150);
-      };
-    
-      if (!projects || projects.length === 0) {
-        return <div className="text-center py-8 text-lightGrey">No projects found.</div>;
-      }
-    
-      // Determine how many projects to show
-      const projectsToShow = showMore ? 6 : 3;
-      const displayedProjects = projects.slice(0, projectsToShow);
-      const hasMoreProjects = projects.length > 3;
+  console.log(projects);
+
+  const fetchAllProjects = async () => {
+    try {
+      const res = await fetchProjects();
+      setProjects(res);
+    } catch (error) {
+      console.error("Failed to fetch projects", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchAllProjects();
+  }, []);
+
+  const handleToggleProjects = () => {
+    setIsTransitioning(true);
+    setTimeout(() => {
+      setShowMore(!showMore);
+      setIsTransitioning(false);
+    }, 150);
+  };
+
+
+  // Determine how many projects to show
+  const projectsToShow = showMore ? 6 : 3;
+  const displayedProjects = projects.slice(0, projectsToShow);
+  const hasMoreProjects = projects.length > 3;
 
   return (
-    <div className={`px-6 py-12 w-full transition-all duration-500 ease-in-out ${
-      showMore ? 'min-h-[140vh]' : 'min-h-[70vh]'
-    }`}>
+    <div
+      className={`px-6 py-12 w-full transition-all duration-500 ease-in-out ${
+        showMore ? "min-h-[140vh]" : "min-h-[70vh]"
+      }`}
+    >
       <SectionHeading title="PROJECTS" />
-      
+
       {/* Projects Grid */}
-      <div className={`grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 transition-all duration-500 ease-in-out ${
-        isTransitioning ? 'opacity-70' : 'opacity-100'
-      }`}>
-        {displayedProjects.map((project, index) => (
-          <div 
-            key={project._id || index}
-            className={`transition-all duration-500 ease-in-out ${
-              index >= 3 && showMore ? 'animate-fade-in' : ''
-            }`}
-          >
-            <ProjectCard project={project} />
-          </div>
-        ))}
-      </div>
-      
+      {!projects ? (
+        <Loading />
+      ) : (
+        <div
+          className={`grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 transition-all duration-500 ease-in-out ${
+            isTransitioning ? "opacity-70" : "opacity-100"
+          }`}
+        >
+          {displayedProjects.map((project, index) => (
+            <div
+              key={project._id || index}
+              className={`transition-all duration-500 ease-in-out ${
+                index >= 3 && showMore ? "animate-fade-in" : ""
+              }`}
+            >
+              <ProjectCard project={project} />
+            </div>
+          ))}
+        </div>
+      )}
+
       {/* Show More/Less Button */}
       {hasMoreProjects && (
         <div className="flex justify-center  mt-4 w-full overflow-hidden">
@@ -74,16 +79,17 @@ const FeaturedProjects = () => {
             onClick={handleToggleProjects}
             disabled={isTransitioning}
             className={`flex items-center justify-center bg-linear-to-t from-classicGold/50 to-richBlack text-xl cursor-pointer w-1/3 border border-darkGrey rounded-full${
-              isTransitioning ? 'scale-95' : ''
+              isTransitioning ? "scale-95" : ""
             }`}
           >
             {showMore ? (
-             
-              <Link href={'/projects'} className="flex justify-center items-center">
+              <Link
+                href={"/projects"}
+                className="flex justify-center items-center"
+              >
                 View All Projects
                 <ChevronRight className="w-5 h-5 ml-2 group-hover:-translate-y-1 transition-transform duration-300" />
               </Link>
-      
             ) : (
               <>
                 <ChevronDown className="w-5 h-5 mr-2 group-hover:translate-y-1 transition-transform duration-300" />
@@ -93,7 +99,7 @@ const FeaturedProjects = () => {
           </button>
         </div>
       )}
-      
+
       {/* Project Counter */}
       <div className="flex justify-center mt-6">
         <span className="text-sm text-lightGrey/60">
